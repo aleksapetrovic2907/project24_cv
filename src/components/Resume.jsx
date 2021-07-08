3b30c448107ab7f496cb9c08ba92bb8efea52d77
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-07-08 21:34:24 +0200

    init

---
+++ src/components/Resume.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Resume extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <div class="card">
+          <div class="card-body">
+            <h1>Test</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Resume;
